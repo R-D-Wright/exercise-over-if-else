bc7fcd21161cf27d2e58614159a78d8897b034c7
Drake Wright <rdrakewright@gmail.com>
2023-02-10 08:40:06 -0600

    Initial Commit

---
+++ If else exercise.playground/Contents.swift
@@ -0,0 +1,25 @@
+let turtle = ""
+if (turtle == "Leonardo"){
+    print("Katana")
+} else if (turtle == "Donatello"){
+    print("Bo Staff")
+} else if (turtle == "Raphael"){
+    print("Twin Sai")
+} else if (turtle == "Michaelangelo"){
+    print("Duel Nunchaku")
+}
+else{
+    print("Heros in a half shell. Turtle Power!")
+}
+
+
+let turtleVillians = ""
+
+switch turtleVillians {
+case "Shredder":
+    print("Tonight I dine on turtle soup")
+case "Krang":
+    print("I HATE Turtles, I despise Turtles")
+default:
+    print("Oh no! It must be the Foot Clan")
+}
